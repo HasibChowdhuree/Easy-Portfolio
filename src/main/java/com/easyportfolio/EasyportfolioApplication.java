94b82f447da713fb2e50226f0a231734da199555
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-06-21 01:47:17 +0600

    Created Spring Boot Project
    Created the project with these dependencies - Spring Web, Spring Security, JPA, Thymeleaf, Devtools, PostgreSQL. Also configured the application.properties file to connect the database.

---
+++ src/main/java/com/easyportfolio/EasyportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.easyportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyportfolio/easyportfolio/EasyportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyportfolio.easyportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
