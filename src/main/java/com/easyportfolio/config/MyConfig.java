58eebca9d48dce3ab59b484027ffb1f9fbee1f31
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-07-06 14:29:34 +0600

    Updated MyConfig.java
    Updated MyConfig.java File so that if users sign in, they are directly sent to user/dashboard page instead of the homepage

--- src/main/java/com/easyportfolio/config/MyConfig.java
+++ src/main/java/com/easyportfolio/config/MyConfig.java
@@ -36,7 +36,7 @@ public class MyConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/user/**").hasRole("USER")
-		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").and().csrf().disable();
+		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").defaultSuccessUrl("/user/dashboard").and().csrf().disable();
 		http.csrf().disable();
 	}
 }
